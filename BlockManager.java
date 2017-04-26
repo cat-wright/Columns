6305558ba890e93ccadd55cf009bcc89a2ffa1f1
Catherine Wright <catherine.jmw@gmail.com>
2017-04-25 23:14:53 -0600

    working blockmanager

--- BlockManager.java
+++ BlockManager.java
@@ -8,6 +8,7 @@
  * April 24th 2017
  */
 
+import java.awt.Color;
 import java.util.*;
 import java.util.Random;
 
@@ -56,13 +57,23 @@ public class BlockManager {
       for(int i = 0; i < 3; i++) { System.out.println(colArray[i]); }
   }
   
+  protected Color getColor(char c) {
+      if(c == '.') return Color.BLACK;
+      if(c == '*') return Color.MAGENTA;
+      if(c == '#') return Color.LIGHT_GRAY;
+      if(c == '$') return Color.GREEN;
+      if(c == '&') return Color.CYAN;
+      if(c == '@') return Color.ORANGE;
+      return Color.WHITE;
+  }
+  
   /**
    * generates a new board
    * @param rows: number of rows in board[][]
    * @param cols: number of cols in board[][]
    * @param colors: int amount of how many characters to use
    */
-  private void generateBoard(int rows, int cols, int colors) {
+  protected void generateBoard(int rows, int cols, int colors) {
       board = new char[rows][cols];
       numColors = colors;
   }
@@ -70,13 +81,13 @@ public class BlockManager {
   /**
    * Sets the board to empty
    */
-   private void emptyBoard() {
+   protected void emptyBoard() {
        for(int i = 0; i < ROWS; i++) {
            for(int j = 0; j < COLS; j++) {
                board[i][j] = EMPTY;
            }
        }
-       printBoard();
+       //printBoard();
    }
    
    /**
@@ -94,6 +105,10 @@ public class BlockManager {
        }
        bStr = sB;
        System.out.println(bStr);
+   }
+   
+   protected char[][] getBoard() {
+       return board;
    }
    
    /*@Override
@@ -127,8 +142,15 @@ public class BlockManager {
        board[firstRow][drop] = colArray[2];
    }
    
+   /**
+    * checks horizontal, vertical, and diagonals for 3 or more in a row
+    * @return boolean returned true if there is at least one matching set
+    * 
+    * I will refactor this smaller hopefully for the game
+    */
    private boolean checkWin() {
        boolean flag = false;
+       //
        for(int i = 0; i < ROWS; i++) {
            for(int j = 0; j < COLS-2; j++) {
                if(board[i][j] != EMPTY) {
@@ -162,43 +184,6 @@ public class BlockManager {
                }
            }
        }
-       return flag;
-   }
-   
-   /*private boolean checkVert() {
-       boolean flag = false;
-       for(int j = 0; j < COLS; j++) {
-           for(int i = 0; i < ROWS-2; i++) {
-               if(board[i][j] != EMPTY) {
-                   if(board[i][j] == board[i+1][j] && board[i+1][j] == board[i+2][j]) {
-                       sameRows.add(i); sameRows.add(i+1); sameRows.add(i+2); 
-                       sameCols.add(j);  sameCols.add(j);  sameCols.add(j); 
-                       flag = true;
-                   }
-               }
-           }
-       }
-       return flag;
-   }
-   
-   private boolean checkLDiag() {
-       boolean flag = false;
-       for(int i = 0; i < ROWS-2; i++) {
-           for(int j = 0; j < COLS-2; j++) {
-               if(board[i][j] != EMPTY) {
-                   if(board[i][j] == board[i+1][j+1] && board[i+1][j+1] == board[i+2][j+2]) {
-                       sameRows.add(i); sameRows.add(i+1); sameRows.add(i+2);
-                       sameCols.add(j); sameCols.add(j+1); sameCols.add(j+2);
-                       flag = true;
-                   }
-               }
-           }
-       }
-       return flag;
-   }
-   
-   private boolean checkRDiag() {
-       boolean flag = false;
        for(int i = ROWS-1; i >= 1 ; i--) {
            for(int j = 0; j < COLS-2; j++) {
                if(board[i][j] != EMPTY) {
@@ -211,22 +196,28 @@ public class BlockManager {
            }
        }
        return flag;
-   }*/
+   }
    
    /**
     * @return true if any blocks will be removed
     */
    protected boolean willRemove() {
        boolean win = checkWin();
-       /*boolean horiz = checkHoriz();
-       boolean vert = checkVert();
-       boolean lDiag = checkLDiag();
-       boolean rDiag = checkRDiag();
-       if(horiz || vert || lDiag || rDiag) return true;*/
        if(win) return true;
        else { 
            return false; 
        }
+   }
+   
+   protected boolean inBoard(int x, int y) {
+       for(int i=0; i<ROWS; i++) {
+           for(int j = 0; j < COLS; j++) {
+               if(x == i && y == j) {
+                   return true;
+               }
+           }
+       }
+       return false;
    }
    
    /**
@@ -245,6 +236,10 @@ public class BlockManager {
        return blocks;
    }
    
+   protected char getPieceAt(int x, int y) {
+       return board[x][y];
+   }
+   
    /**
     * Checks for floating characters and moves them down
     */
@@ -255,7 +250,7 @@ public class BlockManager {
            for(int i = 0; i < ROWS; i++) {
                for(int j = 0; j < COLS; j++) {
                    if(board[i][j] != EMPTY) {
-                       if(i+1 < ROWS && board[i+1][j]== EMPTY) {
+                       if(i+1 < ROWS && board[i+1][j] == EMPTY) {
                            board[i+1][j] = board[i][j];
                            board[i][j] = EMPTY;
                            flag = true;
